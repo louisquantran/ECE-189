8ae6021d7824603a69eeb650a9869e6686eade10
louisquantran <louis.quantran@gmail.com>
2025-11-04 09:30:05 -0800

    Update Fetch implementation

--- phase_1/Fetch.sv
+++ phase_1/Fetch.sv
@@ -23,22 +23,27 @@
 module Fetch(
     input logic clk,
     input logic reset,
-        
-    input logic ready_in,
+    
+    // Downstream    
     input logic [31:0] PC_in,
+    
+    // Upstream
+    input logic ready_in,
     output logic [31:0] instr_out,
     output logic [31:0] PC_out,
     output logic [31:0] PC_4,
-    output logic valid_out
+    output logic valid_out,
 );
     logic [31:0] PC_buf;
     logic [31:0] instr_buf;
-    logic valid_out_sig;
+    logic valid_out_buf;
     logic [31:0] PC_icache;
     logic [31:0] instr_icache;
     
     // Call ICache
     ICache ICache_dut (
+        .clk(clk),
+        .reset(reset),
         .address(PC_in),
         .instruction(instr_icache)
     );
@@ -46,19 +51,19 @@ module Fetch(
     // Combinational assignments
     assign PC_out = PC_buf;
     assign PC_4 = PC_buf + 32'd4;
-    assign valid_out = valid_out_sig;
+    assign valid_out = valid_out_buf;
     assign instr_out = instr_buf;
     
     // Sequential assignments
     always_ff @(posedge clk) begin
         if (reset) begin
-            valid_out_sig <= 1'b0;  
+            valid_out_buf <= 1'b0;  
             instr_buf <= 32'b0;
             PC_buf <= 32'b0;
         end else begin
             PC_icache <= PC_in;
-            if (!valid_out_sig || (valid_out_sig && ready_in)) begin
-                valid_out_sig <= 1'b1;
+            if (!valid_out_buf || ready_in) begin
+                valid_out_buf <= 1'b1;
                 PC_buf <= PC_icache;
                 instr_buf <= instr_icache;
             end
