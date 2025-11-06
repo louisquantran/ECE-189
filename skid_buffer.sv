0096a989d2e4d64e96157e02d8caff12af5aa353
louisquantran <louis.quantran@gmail.com>
2025-11-05 23:44:06 -0800

    Update skid buffer to become a generic module to be able to use across any stage

--- skid_buffer.sv
+++ skid_buffer.sv
@@ -1,59 +1,35 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/29/2025 10:59:10 AM
-// Design Name: 
-// Module Name: skid_buffer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module skid_buffer_struct
-#(
-    parameter type T = logic
+module skid_buffer#(
+    parameter type T = logic [31:0]
 )(
     input logic clk,
     input logic reset,
     
-    // upstream (producer -> skid)
+    // Upstream
     input logic valid_in,
-    output logic ready_in,
     input T data_in,
+    output logic ready_in,
     
-    // downstream (skid -> consumer)
-    output logic valid_out,
+    // Downstream
     input logic ready_out,
-    output T data_out 
+    output logic valid_out,
+    output T data_out
 );
-    T temp = 0;
-    logic full = 0;
+    T data_out_hold;
+    logic full;
     
     assign ready_in = ~full;
     assign valid_out = (full) ? 1'b1 : valid_in;
-    assign data_out = (full) ? temp : data_in;
-    
-    always_ff @(posedge clk) begin 
+    assign data_out = (full) ? data_out_hold : data_in;
+    always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
+            data_out_hold <= '0;
             full <= 1'b0;
-            temp <= '0;
         end else begin
-            // store
             if (!ready_out && valid_in && !full) begin
-                temp <= data_in;
                 full <= 1'b1;
-            // take this out
+                data_out_hold <= data_in;
             end else if (ready_out && full) begin
                 full <= 1'b0;
             end
