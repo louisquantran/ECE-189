2494de632df0ce0d4c496413f0335cf24c73b3ef
louisquantran <louis.quantran@gmail.com>
2025-11-14 00:16:56 -0800

    Clean up priority_decoder comments

--- priority_decoder.sv
+++ priority_decoder.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/29/2025 09:37:27 AM
-// Design Name: 
-// Module Name: priority_decoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module priority_decoder 
 #(
